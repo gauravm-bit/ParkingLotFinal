42f94c32fa17c890947b7bc88451c96033e4c936
gauravm-bit <gauravwrk01@gmail.com>
2020-04-28 11:53:05 +0530

    ADDED:function to pass the test-case.

---
+++ app/ParkingLot.js
@@ -0,0 +1,11 @@
+class parkingLot{
+    constructor(){
+        this.Lot = [];
+    }
+
+    doParking = (car) => {
+        this.Lot.push(car)
+        return true;
+    }
+}
+module.exports = new parkingLot 
