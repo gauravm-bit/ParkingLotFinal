1cbba04bd839e1d95145d04cc8a0044d5185d59b
gauravm-bit <gauravwrk01@gmail.com>
2020-04-28 12:06:40 +0530

    ADDED: test-case to disallow parking if already parked

--- test/ParkingLotTest.js
+++ test/ParkingLotTest.js
@@ -4,10 +4,16 @@ const parkingLot = require('../app/ParkingLot.js')
 let car = { }
 
 describe(`Testing for Parking Lot service`, () => {
-//TC 1.1 let the driver park so that he can board his flight
+//TC 1.1 let the driver park the car so that he can board his flight
 it(`allow parking so the driver can board flight`, () => {
     let result = parkingLot.doParking(car)
     assert.isTrue(result)
 })
+
+//TC 1.2 if the car is already parked, system should not allow  to park again
+it(`dont allow parking if already parked`, () => {
+    let result = parkingLot.doParking(car)
+    assert.isFalse(result)
+ })
 
 }) 
