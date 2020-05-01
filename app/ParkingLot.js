e6d451c5bcc3ac0a6808066fcc3226d0600b9489
gauravm-bit <gauravwrk01@gmail.com>
2020-05-01 10:11:59 +0530

    REFACTORED: searching car function

--- app/ParkingLot.js
+++ app/ParkingLot.js
@@ -14,8 +14,7 @@ class parkingLot{
             throw new Error("Parking lot is full");
         }
         else {
-             var found = this.Lot.some(element => element.type == car.type )
-             if (found) {
+             if (this.search(car)) {
                 throw new Error("Aready parked,no new spot will be alloted");
              }
              else {
@@ -29,8 +28,7 @@ class parkingLot{
         if(this.isEmpty()){
             throw new Error("Parking lot is empty");
         }
-        var found = this.Lot.some(element => element.type == car.type)
-        if (found) {
+        if (this.search(car)) {
             let index = this.Lot.indexOf(car)
             this.Lot.splice(index,1)
             return true;
@@ -62,11 +60,19 @@ class parkingLot{
        }
 
     findCar = (car) => {
-        var found = this.Lot.some(element => element.type == car.type)
-        if (found) {
+        if (this.search(car)) {
             return true
         }
         throw new Error("Car is not present in the Lot");
     }
+
+    //function to search car in the lot
+    search = (car) => {
+        if(this.Lot.some(element => element.type == car.type)){
+            return true
+        }
+        return false  
+    }
+    
 }
 module.exports = new parkingLot 
